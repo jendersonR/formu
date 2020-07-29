a0b1422404090cee64ec8853a6c9cda55cf888e2
jendersonR <68754837+jendersonR@users.noreply.github.com>
2020-07-29 15:36:05 -0500

    Add files via upload

---
+++ correos.php
@@ -0,0 +1,14 @@
+<?php
+$destinatario = 'jendersonrodriguezgonzalez@gmail.com';
+$nombre = $_POST['nombre'];
+$asunto = $_POST['asunto'];
+$mensaje = $_POST['mensaje'];
+$email = $_POST['email'];
+
+$header = "enviado desde la pagina Ag designer";
+$mensajeCompleto = $mensaje . "\nAtentamente: " . $nombre;
+
+mail($destinatario, $asunto, $mensajeCompleto, $header);
+echo "<script>alert('correo enviado')<\script>";
+echo "<script> setTimeout(\"location.href='index.html'\",1000)<\script>";
+?>
